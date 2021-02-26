1eccad2ea44e72ae23574b178f14444f332e6ff0
mohit kumar <vishalkumar123mk@gmail.com>
2021-02-26 11:30:04 +0530

    bootstrap error fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap";
 import { BrowserRouter} from 'react-router-dom'
 ReactDOM.render(
   <React.StrictMode>
